3fa403869b0063816ef6b7aec4312041e60e01d0
victoriabillingsley <vnbillingsley1983@gmail.com>
2014-11-19 16:15:16 -0800

    completed song that doesnt end

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,15 @@
 class PagesController < ApplicationController
 
+  def index
+  end
+
+  def verse_2
+  end
+
+  def verse_3
+  end
+
+  def verse_4
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root to: "pages#index"
+
+  get 'index'   => "pages#index"
+  get 'verse_2' => "pages#verse_2"
+  get 'verse_3' => "pages#verse_3"
+  get 'verse_4' => "pages#verse_4"
 end
